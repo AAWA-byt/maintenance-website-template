841fb361103d7f27db44327173deacadb3bca96b
AAWA-byt <mail@aaronwagner.earth>
2022-10-02 19:46:23 +0200

    :sparkles: added picture, text and copyright link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import maintenance from './maintenance.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We're under maintenance</h1>
+        <img src={maintenance} alt="rocket" />
+        <p className='copyright'><a href="https://www.clipartmax.com/middle/m2i8m2Z5Z5H7i8Z5_website-maintenance-icon-png/">Website Maintenance Icon Png @clipartmax.com</a></p>
       </header>
     </div>
   );
